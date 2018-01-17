69fd52c2d70f175f9f857037c8df48479b104123
takizawad <takizawad@systena.co.jp>
2018-01-17 10:01:32 +0900

    1st commit

---
+++ src/RPSGame.java
@@ -0,0 +1,10 @@
+
+public class RPSGame {
+
+  public static void main(String[] args) {
+    int id = RPSType.PAPER.getId();
+    System.out.println(id);
+    
+  }
+
+}
---
+++ src/RPSPlayerBase.java
@@ -0,0 +1,4 @@
+
+public abstract class RPSPlayerBase {
+  public abstract RPSType go();
+}
